c421f52542757b3feb32538e9b1b1be921ae91e5
Jeff <jeff@goodboydigital.com>
2017-08-23 17:43:09 +0100

    fixed movments

--- Assets/Scripts/MouseFollow.cs
+++ Assets/Scripts/MouseFollow.cs
@@ -6,9 +6,12 @@ public class MouseFollow : MonoBehaviour {
 
     private Vector3 mousePosition;
     private float currentRotation;
-    public float moveSpeed = 0.1f;
+    public float moveSpeed = 0;
+    public float maxSpeed = 0.1f;
+    public float acc = 0.01f;
     public float rotationSpeed = 0.1f;
     private Vector2 velocity;
+    private float stopTimer;
     public int side;
     // Use this for initialization
     void Start()
@@ -19,12 +22,24 @@ public class MouseFollow : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
+        bool touched = Input.GetMouseButton(0);
 
-            side = 1;
+        if (touched)
+        {
             mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            if (Vector2.Distance(mousePosition, transform.position) < 0.5)
+            {
+                touched = false;
+            }
+        }
+
+        if (touched)
+        {
+            stopTimer = 1;
+            side = 1;
+            
 
             //print(mousePosition.x +" - "+ this.transform.position.x);
 
@@ -36,24 +51,22 @@ public class MouseFollow : MonoBehaviour {
 
 
             float tan = Mathf.Atan2(mousePosition.y - transform.position.y, mousePosition.x - transform.position.x);
+            
             Vector2 nextVel = new Vector2();
             nextVel.x = Mathf.Cos(tan) * moveSpeed;
             nextVel.y = Mathf.Sin(tan) * moveSpeed;
 
-            velocity = Vector2.Lerp(velocity, nextVel, moveSpeed);
+
+            moveSpeed = Mathf.Lerp(moveSpeed, maxSpeed, acc);
+
+            velocity = nextVel;// Vector2.Lerp(velocity, nextVel, moveSpeed);
 
 
-            
-
-            print(tan);
-            print(velocity);
 
 
-            Vector2 nextPos = transform.position;
-            nextPos += velocity;
-            transform.position = nextPos;
-
-
+            //print(tan);
+            //print(velocity);            
+            print("ON");
 
             //currentRotation = transform.rotation.z;
             currentRotation += rotationSpeed * tan;
@@ -66,6 +79,16 @@ public class MouseFollow : MonoBehaviour {
             //print(transform.rotation);
             //print(currentRotation);
         }
+        else
+        {
+            stopTimer = Mathf.Lerp(stopTimer, 0, acc*10000);
+            print("OFF   " + stopTimer);
+            velocity *= stopTimer;
+        }
+
+        Vector2 nextPos = transform.position;
+        nextPos += velocity;
+        transform.position = nextPos;
 
     }
 }
